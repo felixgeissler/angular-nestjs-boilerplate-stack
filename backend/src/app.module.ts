e06c7cf569a09eea8f7ba3ec7d677ce809d777db
Felix Geißler <felix.geissler.94@gmail.com>
2020-09-29 16:53:26 +0200

    Setup TypeORM, Nodemon & Dotenv for NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
